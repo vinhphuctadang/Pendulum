4ecc1fda3bff8ab6ab8e0ad12f94fde19e9cb5b9
Vinh Phuc Ta Dang <vinhphuctadang@gmail.com>
2020-10-04 03:03:57 +0700

    hello qt commit

---
+++ test.py
@@ -0,0 +1,47 @@
+'''
+Draw simple pendulum
+'''
+
+import log
+
+import sys
+from PySide2 import QtGui, QtCore, QtWidgets
+
+class Example(QtWidgets.QWidget):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):      
+
+        self.text = u'Hello world'
+
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('Draw text')
+        self.show()
+
+    def paintEvent(self, event):
+
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.drawRectange(0, 0, 100, 100)
+        qp.end()
+        
+    def drawText(self, event, qp):
+      
+        qp.setPen(QtGui.QColor(168, 34, 3))
+        qp.setFont(QtGui.QFont('Decorative', 10))
+        qp.drawText(event.rect(), QtCore.Qt.AlignCenter, self.text)        
+                
+        
+def main():
+    
+    app = QtWidgets.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
